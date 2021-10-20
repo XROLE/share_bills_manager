b8500d087c33455d64b89b0c8b1f41103c1a4e2f
xrole <xrolediamond@gmail.com>
2021-10-20 09:49:18 +0100

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_bills_manager/features/Authentication/signup.dart';
 
-import 'features/home/presentation/index.dart';
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+/// We are using a StatefulWidget such that we only create the [Future] once,
+/// no matter how many times our widget rebuild.
+/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
+/// would re-initialize FlutterFire and make our application re-enter loading state,
+/// which is undesired.
+
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+    final Future<void> _initializeFirebase =  Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +29,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SignUp(),
-    );
+      home: FutureBuilder(
+        future: _initializeFirebase,
+        builder: (context, snapshot){
+          if(snapshot.hasError){
+            print('Something went wrong ====================> ${snapshot.error}');
+            return Center(child: Text('Something went wrong'));
+          } else if(snapshot.connectionState == ConnectionState.done){
+            print('Loaded successfully');
+        return SignUp();
+          } else {
+            print('======================= I am a chosen one');
+            return Center(child: CupertinoActivityIndicator(),);
+          }
+        }
+    ));
   }
 }
