98abd13d7fd3f704d87b08620c9d84b50002cea6
xrole <xrolediamond@gmail.com>
2021-10-20 18:06:17 +0100

    remove unused with variable from signup page create password validator method add test for password validator method add password validator constant string

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,4 +1,5 @@
 class Constants {
   static const String EMPTY_MAIL_STRING = 'Email can not be empty';
   static const String INVALID_MAIL_STRING = 'Invalid email';
+  static const String INVALID_PASSWORD_LENGHT = 'Password must be atleast six (6) character\' long';
 }
