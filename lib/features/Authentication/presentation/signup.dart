609e674b46e9301f65feaa13ab3ac3356ea26d4b
xrole <xrolediamond@gmail.com>
2021-11-01 02:47:18 +0100

    remove old implementation of sign up business logic

--- lib/features/Authentication/presentation/signup.dart
+++ lib/features/Authentication/presentation/signup.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shared_bills_manager/features/Authentication/data/datasource/index.dart';
 import 'package:shared_bills_manager/features/Authentication/presentation/bloc/sign_up_bloc.dart';
 import 'package:shared_bills_manager/features/Authentication/presentation/bloc/sign_up_event.dart';
 import 'package:shared_bills_manager/features/Authentication/presentation/bloc/sign_up_states.dart';
